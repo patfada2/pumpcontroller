65ba0aeae7f47dfa09a44dbaf6021b1715ca6ca2
patfada2 <patfada@gmail.com>
2025-08-01 16:17:26 +1200

    refactor wifi

--- PumpController/PumpController.ino
+++ PumpController/PumpController.ino
@@ -1,21 +1,12 @@
 #include <LittleFS.h>
 
-#ifdef ESP32
-#include <WiFi.h>
-#include <ESPAsyncWebServer.h>
-#include <LittleFS.h>
-#include <FS.h>
-#else
+
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <Hash.h>
-#include <ESPAsyncTCP.h>
-#include <ESPAsyncWebServer.h>
+
 
 #include <ArduinoJson.h>
-#include <ESP8266WiFi.h>
 
-#endif
+
 #include <Wire.h>
 
 #include <ctime>
@@ -28,6 +19,7 @@
 #include "./timeutils.h"
 #include "./fileutils.h"
 #include "./lcdutils.h"
+#include "./wifiutils.h"
 #include <TimeLib.h>
 
 
@@ -54,9 +46,7 @@ std::string data;
 const int relay1Pin = D4;  //!! check board wiring
 const int relay2Pin = 0;  //d??
 const int AC_DETECT = 0;  //need to assign a digital inpu to this
-// Replace with your network credentials
-const char* ssid = "RosieWiFi";
-const char* password = "Thr33.0n3";
+
 const int analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0
 String dataFile0 = "/voltageHistory.txt";
 String dataFile1 = "/stateHistory.txt";
@@ -189,35 +179,6 @@ void setupLittleFS() {
 
   listAllFilesInDir("/");
 }
-
-void setupWiFi() {
-
-  // Connect to Wi-Fi
-  // Set your Static IP address
-  IPAddress local_IP(192, 168, 1, 99);
-  // Set your Gateway IP address
-  IPAddress gateway(192, 168, 1, 254);
-
-  IPAddress subnet(255, 255, 255, 0);
-  IPAddress primaryDNS(8, 8, 8, 8);    //optional
-  IPAddress secondaryDNS(8, 8, 4, 4);  //optional
-
-  // Configures static IP address
-  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
-    Serial.println("STA Failed to configure");
-  }
-
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
-  }
-
-  // Print ESP32 Local IP Address
-  Serial.println(WiFi.localIP());
-}
-
-
 
 void setupWebServer() {
   // Route for root / web page
